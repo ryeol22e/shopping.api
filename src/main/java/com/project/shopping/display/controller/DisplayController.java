87ffa955a28bab9dc8c6b9df3680f0cfa2c92747
jaeryeol <ryeol22e@gmail.com>
2022-09-25 20:36:00 +0900

    display 작업

---
+++ src/main/java/com/project/shopping/display/controller/DisplayController.java
@@ -0,0 +1,13 @@
+package com.project.shopping.display.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/display")
+@RequiredArgsConstructor
+public class DisplayController {
+	
+}
---
+++ src/main/java/com/project/shopping/display/service/DisplayService.java
@@ -0,0 +1,11 @@
+package com.project.shopping.display.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class DisplayService {
+	
+}
