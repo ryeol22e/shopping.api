b1594b7dfabf93aded1e9c2f8182b7ded12a2745
jaeryeol <ryeol22e@gmail.com>
2022-10-17 20:45:38 +0900

    @EnableEncryptableProperties 추가

--- src/main/java/com/project/shopping/ShoppingApplication.java
+++ src/main/java/com/project/shopping/ShoppingApplication.java
@@ -3,7 +3,10 @@ package com.project.shopping;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
 @SpringBootApplication
+@EnableEncryptableProperties
 public class ShoppingApplication {
 
 	public static void main(String[] args) {
