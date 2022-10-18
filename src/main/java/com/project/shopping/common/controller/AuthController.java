53b09347b1051589c12d85754ab22f719ec5e389
jaeryeol <ryeol22e@gmail.com>
2022-10-18 22:01:16 +0900

    수정

--- src/main/java/com/project/shopping/common/controller/AuthController.java
+++ src/main/java/com/project/shopping/common/controller/AuthController.java
@@ -7,9 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
@@ -17,7 +15,6 @@ public class AuthController {
 	@GetMapping("/check")
 	public ResponseEntity<Boolean> authCheck(HttpServletRequest request) throws Exception {
 		String token = request.getHeader(HttpHeaders.AUTHORIZATION);
-		log.info("token : {}", token);
 		return ResponseEntity.ok(token==null || "".equals(token) ? false : true);
 	}
 }
