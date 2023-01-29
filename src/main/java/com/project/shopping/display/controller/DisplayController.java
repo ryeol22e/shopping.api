154834e0b87201c8f5278bfa71f5fd22ab761a07
jaeryeol <ryeol22e@gmail.com>
2023-01-29 20:17:12 +0900

    add refactoring

--- src/main/java/com/project/shopping/display/controller/DisplayController.java
+++ src/main/java/com/project/shopping/display/controller/DisplayController.java
@@ -22,17 +22,17 @@ public class DisplayController {
 	private final ProductService productService;
 
 	@GetMapping("/main/banner")
-	public ResponseEntity<List<BannerDTO>> getMainBanner(BannerDTO bannerDTO) throws Exception {
+	public ResponseEntity<List<BannerDTO>> getMainBanner(BannerDTO bannerDTO) {
 		return ResponseEntity.ok(displayService.getBannerList(bannerDTO));
 	}
 
 	@GetMapping("/main/corner")
-	public ResponseEntity<String> getMainCorner() throws Exception {
+	public ResponseEntity<String> getMainCorner() {
 		return ResponseEntity.ok("");
 	}
 
 	@GetMapping("/product/list")
-	public ResponseEntity<List<ProductDTO>> getProductList(ProductDTO productDTO) throws Exception {
+	public ResponseEntity<List<ProductDTO>> getProductList(ProductDTO productDTO) {
 		return ResponseEntity.ok(productService.getProductList(productDTO));
 	}
 }
