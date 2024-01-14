359559048189c930465f2e83c1d844d1cfc83e4e
jaeryeol <ryeol22e@gmail.com>
2024-01-14 16:10:46 +0900

    delete log

--- src/main/java/com/project/shopping/product/service/ProductService.java
+++ src/main/java/com/project/shopping/product/service/ProductService.java
@@ -5,16 +5,13 @@ import org.springframework.stereotype.Service;
 import com.project.shopping.product.dto.ProductDTO;
 import com.project.shopping.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 	private final ProductRepository productRepository;
 
 	public List<ProductDTO> getProductList(ProductDTO productDTO) {
-		log.info(":::::::::::::::::::::::::::::::::::::::::::::::::::::: {}", productDTO.getLastPrdtNo());
 		return productRepository.findProductList(productDTO);
 	}
 
