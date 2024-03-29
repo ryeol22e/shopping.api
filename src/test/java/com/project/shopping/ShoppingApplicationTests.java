07a3b42fd09e3b75cf75323459f571803cc8fe9c
ryeol22e <ryeol22e@gmail.com>
2024-03-29 23:27:15 +0900

    gradle, test수정

--- src/test/java/com/project/shopping/ShoppingApplicationTests.java
+++ src/test/java/com/project/shopping/ShoppingApplicationTests.java
@@ -3,11 +3,10 @@ package com.project.shopping;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ShoppingApplicationTests.class)
 class ShoppingApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+	void contextLoads() {}
 
 }
