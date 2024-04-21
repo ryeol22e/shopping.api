d038ea92eb4c1f47dbf881231fda997fca2107c8
ryeol22e <ryeol22e@gmail.com>
2024-04-21 19:31:59 +0900

    cacheable

--- src/main/java/com/project/shopping/display/service/DisplayService.java
+++ src/main/java/com/project/shopping/display/service/DisplayService.java
@@ -2,13 +2,10 @@ package com.project.shopping.display.service;
 
 import java.time.LocalDateTime;
 import java.util.List;
-
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
-
 import com.project.shopping.display.dto.BannerInfo;
 import com.project.shopping.display.repository.BannerRepository;
-import com.project.shopping.utils.UtilsData;
-
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -16,20 +13,13 @@ import lombok.RequiredArgsConstructor;
 public class DisplayService {
 	private final BannerRepository bannerRepository;
 
+	@Cacheable(value = "getBannerList", key = "#dto?.bannerType")
 	public List<BannerInfo> getBannerList(BannerInfo dto) {
 		LocalDateTime today = LocalDateTime.now();
 		List<BannerInfo> list = bannerRepository.findByBannerList(dto).stream()
 				.filter(x -> today.compareTo(x.getDispStartDtm()) >= 0
 						&& today.compareTo(x.getDispEndDtm()) <= 0)
 				.toList();
-
-		for (int i = 0, size = list.size(); i < size; i++) {
-			BannerInfo banner = list.get(i);
-
-			if (banner.getImageData() != null) {
-				banner.setImage(UtilsData.getBlobToByte(banner.getImageData()));
-			}
-		}
 
 		return list;
 	}
